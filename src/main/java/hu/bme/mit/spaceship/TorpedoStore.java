c85ccccb1328a61a95d0b7ed5a0529538e9f1dea
unknown <184841@BME.HU>
2024-10-09 17:10:53 +0200

    komment a kedvedert

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -40,7 +40,7 @@ public class TorpedoStore {
 
     // simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
-
+    //egy random komment a kedvedert
     if (r >= FAILURE_RATE) {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
